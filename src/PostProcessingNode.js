9498f93828e4233f221ff01ad0b3c5de5b731fd1
Thor Merlin Lervik <rohtgnilk@gmail.com>
2018-07-10 19:55:59 +0200

    Add paper texture overlay on all scenes

---
+++ src/PostProcessingNode.js
@@ -0,0 +1,21 @@
+(function(global) {
+  class PostProcessingNode extends NIN.ShaderNode {
+    constructor(id, options) {
+      options.inputs = {
+        tDiffuse: new NIN.TextureInput(),
+      };
+      super(id, options);
+
+      this.paperTexture = Loader.loadTexture("res/paper.jpg");
+      this.paperTexture.wrapS = this.paperTexture.wrapT = THREE.RepeatWrapping;
+    }
+
+    update(frame) {
+      this.uniforms.frame.value = frame;
+      this.uniforms.tDiffuse.value = this.inputs.tDiffuse.getValue();
+      this.uniforms.paperTexture.value = this.paperTexture;
+    }
+  }
+
+  global.PostProcessingNode = PostProcessingNode;
+})(this);
