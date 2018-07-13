406faac08bb320fa6e22327eca139915803170f2
Péter Gombos <peter@gombos.no>
2018-07-13 13:46:39 +0200

    Logo stuff!

---
+++ src/ndv_composotoryNode.js
@@ -0,0 +1,18 @@
+(function(global) {
+  class ndv_composotoryNode extends NIN.ShaderNode {
+    constructor(id, options) {
+    	options.inputs ={ logo : new NIN.TextureInput(),
+    		patterns: new NIN.TextureInput()
+    }
+      super(id, options);
+    }
+
+    update(frame) {
+      this.uniforms.frame.value = frame;
+      this.uniforms.logo.value = this.inputs.logo.getValue();
+      this.uniforms.patterns.value = this.inputs.patterns.getValue();
+    }
+  }
+
+  global.ndv_composotoryNode = ndv_composotoryNode;
+})(this);
