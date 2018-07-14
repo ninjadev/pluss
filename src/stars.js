51c1fac8746cc7f03d72c2ea8f8083d758432eeb
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2018-07-14 03:16:18 +0200

    Improve sync timings

--- src/stars.js
+++ src/stars.js
@@ -66,23 +66,22 @@
       }
 
       let amp = 0.01;
-      if(BEAT) {
-        switch((BEAN - 3984) % (48 * 8)) {
+      if(BEAT && BEAN <= 4272) {
+        switch((BEAN - 3984) % (48 * 4)) {
         case 0:
         case 32 - 4:
         case 48 - 4:
         case 48 + 32 - 4:
         case 48 + 48 - 4:
-        case 48 + 48 + 12 - 4:
-        case 48 + 48 + 24 - 4:
+        case 48 + 48 + 12:
+        case 48 + 48 + 12 + 8:
         case 48 + 48 + 32 - 4:
-        case 48 + 48 + 32:
         case 48 + 48 + 48 - 4:
-        case 48 + 48 + 48:
-        case 48 + 48 + 48 + 12 - 4:
-        case 48 + 48 + 48 + 24 - 4:
-        case 48 + 48 + 48 + 32 - 4:
-        case 48 + 48 + 48 + 32:
+        case 48 * 3:
+        case 48 * 3 + 8:
+        case 48 * 3 + 24 - 4:
+        case 48 * 3 + 32 - 4:
+        case 48 * 3 + 32:
           amp = 0.13;
         }
       }
