283bb2916b2f7bb3a2a7483d283ac5feefe2d715
Olav Markussen <olavmarkussen@hotmail.no>
2018-07-14 16:13:18 +0200

    Add triangle sprinkles to icecreamcone

--- src/circleOlav.js
+++ src/circleOlav.js
@@ -31,7 +31,7 @@
         ctx.fillStyle = 'rgba(0,0,0,1)';
         ctx.fillRect(0, 0, 16, 9);
 
-        const nudge = () => random()*0.3;
+        const nudge = () => random()*2.3;
 
 
         // Sprincle drawing
@@ -40,27 +40,53 @@
           ctx.rotate(frame/100 % 2*Math.PI + random());
           ctx.translate(-0.5,-2);
           ctx.beginPath();
+					/*
           ctx.lineTo(0+nudge(),0+nudge());
           ctx.lineTo(0+nudge(),4+nudge());
           ctx.lineTo(1+nudge(),4+random());
           ctx.lineTo(1+nudge(),0+nudge());
           ctx.closePath();
           ctx.lineWidth = 0.1;
-          ctx.fillStyle = '#fdf143';
+          ctx.fillStyle = '#FFFF00';
           ctx.strokeStyle = '#ff5';
           ctx.stroke();
           ctx.fill();
           ctx.restore();
+					ctx.beginPath();
+					*/
+        	//ctx.fillStyle="#FF0000"
+					ctx.fillStyle="#fcfbe3"
+          ctx.restore();
+          //ctx.rotate(frame/100 % 2*Math.PI + -0.5+random()*-0.5);
+					ctx.moveTo(0, 0);
+					ctx.lineTo(4, 4);
+					ctx.lineTo(8, 0);
+					ctx.fill();
+					ctx.save()
+
+          //ctx.translate(-0.5,-2);
+          ctx.beginPath();
+        	ctx.fillStyle="#FFFFFF"
+					ctx.moveTo(0+nudge(), 0+nudge());
+					ctx.lineTo(4+nudge(), 4+nudge());
+					ctx.lineTo(8+nudge(), 0+nudge());
+					ctx.fill();
+          ctx.restore();
         }
         ctx.scale(0.2, 0.2);
         ctx.translate(1,1);
-        for (const x in [...Array(10).keys()]) {
+
+        var S = Math.sin;
+        var C = Math.cos;
+        ctx.fillStyle="#FF0000"
+
+        for (const x in [...Array(5).keys()]) {
           ctx.save();
           ctx.translate(random()*2,random()*2);
-          for (const y in [...Array(10).keys()]) {
+          for (const y in [...Array(5).keys()]) {
             ctx.save();
             ctx.translate(random()*4,random()*2);
-            ctx.translate(x*8,y*4.3);
+            ctx.translate(x*15,y*8);
             draw_sprinkle();
             ctx.restore();
           }
