648f3418f147fbfe1ab9358a846ac7f0505176ce
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2018-07-14 16:24:35 +0200

    More sync

--- src/squiggleBackgroundNode.js
+++ src/squiggleBackgroundNode.js
@@ -2,10 +2,34 @@
   class squiggleBackgroundNode extends NIN.ShaderNode {
     constructor(id, options) {
       super(id, options);
+      this.amplitude = 0;
     }
 
     update(frame) {
       this.uniforms.frame.value = frame;
+
+      this.amplitude *= 0.9;
+      if(BEAT && BEAN <= 4272) {
+        switch((BEAN - 3600) % (48 * 4)) {
+        case 0:
+        case 32 - 4:
+        case 48 - 4:
+        case 48 + 32 - 4:
+        case 48 + 48 - 4:
+        case 48 + 48 + 12:
+        case 48 + 48 + 12 + 8:
+        case 48 + 48 + 32 - 4:
+        case 48 + 48 + 48 - 4:
+        case 48 * 3:
+        case 48 * 3 + 8:
+        case 48 * 3 + 24 - 4:
+        case 48 * 3 + 32 - 4:
+        case 48 * 3 + 32:
+          this.amplitude = 1;
+        }
+
+        this.uniforms.amplitude.value  = this.amplitude;
+      }
     }
   }
 
